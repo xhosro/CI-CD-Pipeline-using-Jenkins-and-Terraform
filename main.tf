016725d138046ed834a06fafd9d4b6f3dc5b10ec
xhosro <109581911+xhosro@users.noreply.github.com>
2024-05-10 02:43:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "google_storage_bucket" "bucket" {
     force_destroy = true
     public_access_prevention = "enforced"
 }
+
+resource "google_storage_bucket" "bucket-2" {
+    name = "jenkins-tf-gcp-bucket-0012345678"
+    project = "terraform-422613"
+    location = "EU"
+    force_destroy = true
+    public_access_prevention = "enforced"
+}
